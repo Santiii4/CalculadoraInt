a9043801c5de04493cd22d1dab59cb1ea018b086
Santi <sal.sanchez@alumno.etec.um.edu.ar>
2023-11-22 21:04:44 -0300

    22/11

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 from tkinter import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -16,6 +18,9 @@ def Derivar():
         f = parse_expr(funcion_escrita)
         derivada = diff(f, x)
         etiqueta.configure(text=derivada)
+
+        plot_grafico(f, derivada)
+    
     except:
         etiqueta.configure(text="Introduce la función correctamente")
 
@@ -26,6 +31,9 @@ def Integrales_Indefinidas():
         g = parse_expr(funcion_escrita2)
         integral = integrate(g, x)
         etiqueta.configure(text=integral)
+
+        plot_grafico(g)
+
     except:
         etiqueta.configure(text="Introduce la función correctamente")
 
@@ -38,8 +46,29 @@ def Integrales_Definidas():
         limite_superior = float(input("Límite superior: "))
         integral_definida = integrate(g, (x, limite_inferior, limite_superior))
         etiqueta.configure(text=integral_definida)
+
+        plot_grafico(g, limite_inferior, limite_superior)
+
     except:
         etiqueta.configure(text="Introduce la función correctamente")
+
+def plot_grafico(f, limite_inf=None, limite_sup=None):
+    x_vals = np.linspace(-10, 10, 400)
+    y_vals = [f.evalf(subs={'x': x_val}) for x_val in x_vals]
+
+    plt.figure()
+    plt.plot(x_vals, y_vals, label='f(x)')
+
+    if limite_inf is not None and limite_sup is not None:
+        plt.axvline(x=limite_inf, color='r', linestyle='--', label='Límite Inferior')
+        plt.axvline(x=limite_sup, color='g', linestyle='--', label='Límite Superior')
+
+    plt.title('Gráfico de la Función')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
 
@@ -48,7 +77,7 @@ def Integrales_Definidas():
 
 
 ventana = Tk()
-ventana.geometry('300x250')
+ventana.geometry('400x350')
 ventana.title("Cálculo Diferencial e Integral: f(x)")
 
 anuncio = Label(ventana, text="Introduce una función de x:", font=("Arial", 15), fg="blue")
@@ -72,3 +101,4 @@ boton3.pack()
 ventana.mainloop()
 
 # 3*x**2+4*x**2
+# raiz cuadrada = sqrt()
